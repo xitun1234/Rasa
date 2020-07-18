3ef7b24d220b3f431b4b7f5549eb53b616b00961
1512346 <huynhtrongnghia.htn@gmail.com>
2020-07-19 00:17:48 +0700

    update

--- start_services.sh
+++ start_services.sh
@@ -1,6 +1,5 @@
-cd app/
+
 # Start actions server in background
 rasa run actions 
 # Start rasa server with nlu model
-rasa run --model models --enable-api --cors "*" --debug \
-         -p $PORT
+rasa run --model models --enable-api --cors "*" --debug 
