61dc64686a158206e39b337797b3ba0c1ecd7401
1512346 <huynhtrongnghia.htn@gmail.com>
2020-07-19 00:39:11 +0700

    add heroku.yml

--- start_services.sh
+++ start_services.sh
@@ -1,5 +1,6 @@
-
+cd app/
 # Start actions server in background
 rasa run actions 
 # Start rasa server with nlu model
-rasa run --model models --enable-api --cors "*" --debug 
+rasa run --model models --enable-api --cors "*" --debug \
+         -p $PORT
